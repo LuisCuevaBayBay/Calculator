2386b1e29c85457e160c47e55a07c5a40854d9e9
Luis Cueva <luis.cuevas@ujcv.edu.hn>
2018-05-31 08:40:36 -0600

    Fixed code for Main and MenuCalculadora

--- src/com/company/MenuCalculadora.java
+++ src/com/company/MenuCalculadora.java
@@ -6,8 +6,8 @@ public class MenuCalculadora {
 
     private LectorTeclado lt = new LectorTeclado();
 
-    public static void Menu(){
-        int j=0 ;
+    public void Menu() {
+
 
         Scanner sc = new Scanner(System.in);
         System.out.println("Bienvenido a la calculadora");
@@ -26,53 +26,62 @@ public class MenuCalculadora {
         System.out.println("\t 11. Raiz Cubica");
         System.out.println("\t 12. Salir del programa");
 
-        int j2 = sc.nextInt();
-        switch (j2){
-            case 1:
-                Suma suma = new Suma();
-                suma.totalSuma();
-                break;
-            case 2:
-                Resta resta = new Resta();
-                resta.totalResta();
-                break;
-            case 3:
-                Multiplicacion multiplicacion = new Multiplicacion();
-                multiplicacion.Multiplicacion();
-                break;
-            case 4:
-                Division division = new Division();
-                division.TotalDivision();
-                break;
-            case 5:
-                Exponente exponente = new Exponente();
-                exponente.totalExponente();
-                break;
-            case 6:
-                RaizCuadrada raizCuadrada = new RaizCuadrada();
-                raizCuadrada.TotalSQRT();
-                break;
-            case 7:
-                SumaFracciones sumaFracciones = new SumaFracciones();
-                sumaFracciones.SumaFracc();
-                break;
-            case 8:
-                RestaFracciones restaFracciones = new RestaFracciones();
-                restaFracciones.RestaFracc();
-                break;
-            case 9:
-                MultiplicacionFracciones multiplicacionFracciones = new MultiplicacionFracciones();
-                multiplicacionFracciones.MultiFracc();
-                break;
-            case 10:
-                DivisionFracciones divisionFracciones = new DivisionFracciones();
-                divisionFracciones.DivFracc();
-                break;
-            case 11:
-                RaizCubica raizCubica = new RaizCubica();
-                raizCubica.RaizALa3();
-                break;
-        }
-    }
+        int j2;
 
+        do {
+            j2 = lt.leerEntero("Ingrese una opcion", "Opcion no valida");
+
+            switch (j2) {
+                case 1:
+                    Suma suma = new Suma();
+                    suma.totalSuma();
+                    break;
+                case 2:
+                    Resta resta = new Resta();
+                    resta.totalResta();
+                    break;
+                case 3:
+                    Multiplicacion multiplicacion = new Multiplicacion();
+                    multiplicacion.Multiplicacion();
+                    break;
+                case 4:
+                    Division division = new Division();
+                    division.TotalDivision();
+                    break;
+                case 5:
+                    Exponente exponente = new Exponente();
+                    exponente.totalExponente();
+                    break;
+                case 6:
+                    RaizCuadrada raizCuadrada = new RaizCuadrada();
+                    raizCuadrada.TotalSQRT();
+                    break;
+                case 7:
+                    SumaFracciones sumaFracciones = new SumaFracciones();
+                    sumaFracciones.SumaFracc();
+                    break;
+                case 8:
+                    RestaFracciones restaFracciones = new RestaFracciones();
+                    restaFracciones.RestaFracc();
+                    break;
+                case 9:
+                    MultiplicacionFracciones multiplicacionFracciones = new MultiplicacionFracciones();
+                    multiplicacionFracciones.MultiFracc();
+                    break;
+                case 10:
+                    DivisionFracciones divisionFracciones = new DivisionFracciones();
+                    divisionFracciones.DivFracc();
+                    break;
+                case 11:
+                    RaizCubica raizCubica = new RaizCubica();
+                    raizCubica.RaizALa3();
+                    break;
+                case 12:
+                    System.out.println("Gracias por usar el programa ");
+
+            }
+
+        }while (j2 !=12 );
     }
+}
+
